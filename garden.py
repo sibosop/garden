5cbb34ac16b5fa2baf70918025c1d6982d10248d
sibosop <brian@eastshore.com>
2018-10-08 18:13:25 -0700

    added options to jsonPlayback

--- garden.py
+++ garden.py
@@ -8,16 +8,52 @@ import sys
 import random
 import pygame
 import json
+import argparse
+import glob
 
 import time
 import gardenPlayer
 import specs
 
 baseTime = time.time()
+takesDir = ""
+
 
 def usage():
   print "usage:",sys.argv[0]," spec file"
   os._exit(-1)
+  
+def makeTakesDir():
+  global takesDir
+  print "creating takes dir"
+  rootDir = "GardenTakes/*.*"
+  i = 0
+  done = False
+  files = glob.glob(rootDir)
+  while True:
+    found = False
+    for f in files:
+      #print f
+      try:
+        n = f.rindex(".")
+      except:
+        continue
+      test = int(f[n+1:])
+      #print "test:",test
+      if test == i:
+        found = True
+        i += 1
+        break
+    if not found:
+      takesDir = "GardenTakes/Take.%d"%i
+      print "making",takesDir
+      os.mkdir(takesDir)
+      break
+    
+    
+    
+      
+  
 
 if __name__ == '__main__':
   random.seed()
@@ -25,10 +61,18 @@ if __name__ == '__main__':
   os.environ['DISPLAY']=":0.0"
   os.chdir(os.path.dirname(sys.argv[0]))
   print(pname+" at "+datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))  
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', '--spec', nargs=1, help='specify spec file', required=True )
+  parser.add_argument('-o','--output', action = 'store_true',help='save session to GardenTakes directory')
+  args = parser.parse_args()
+  specFile = args.spec[0]
+  print "using spec:",specFile
+  specs.setup(specFile)
+  if args.output:
+    makeTakesDir()
+    
+  print "takesDir:",takesDir
   
-  if len (sys.argv) < 2:
-    usage()
-  specs.setup(sys.argv[1])
   pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=4096)
   pygame.init()
   gardenSoundFile.setup()
@@ -50,10 +94,13 @@ if __name__ == '__main__':
       break;
     time.sleep(1)
     
-  for t in threads:
-    desc = json.dumps(t.playList)
-    f = open(t.name+".json","w")
-    f.write(desc)
-    f.close()
+  if takesDir != "":
+    for t in threads:
+      desc = json.dumps(t.playList)
+      fname = takesDir+"/"+t.name+".json"
+      print "saving:",fname
+      f = open(fname,"w")
+      f.write(desc)
+      f.close()
   print "garden done"
 
--- jsonPlayback.py
+++ jsonPlayback.py
@@ -7,57 +7,144 @@ import datetime
 import gardenTrack
 import pygame
 import random
+import argparse
+import glob
+import threading
 
 debug=True
+numEvents = 0
+baseTime = time.time()
+takesDir = "GardenTakes/"
+threads = []
+
 
 def usage():
   print "usage:",sys.argv[0]," spec file"
   os._exit(-1)
 
-def playSound(sound):
+def playSound(sound,l,r):
   eventChan = None
   eventChan=pygame.mixer.find_channel()
   if eventChan is None:
     pygame.mixer.set_num_channels(pygame.mixer.get_num_channels()+1);
     eventChan=pygame.mixer.find_channel()
   v = random.uniform(.3,.5);
-  eventChan.set_volume(v)
+  lv = l % v
+  rv = r % v
+  print "lv %f rv %f"%(lv,rv)
+  if numEvents == 1:
+    eventChan.set_volume(v)
+  else:
+    eventChan.set_volume(lv,rv)
   eventChan.play(sound)
   eventChan.set_endevent()
   
+class Playback(threading.Thread):
+  def __init__(self,c,specs):
+    global numEvents
+    super(Playback,self).__init__()
+    self.runState = True
+    self.name = "Playback-"+str(c)
+    self.currentDir = os.getcwd()
+    self.specs = specs
+    if numEvents == 2:
+      if c == 1:
+        self.lRatio = 1.0
+        self.rRatio = 0
+      else:
+        self.lRatio = 0
+        self.rRatio = 1.0
+    else:
+      print "numEvents %d thread number %d"%(numEvents,c)
+      self.rRatio = float(c) / float(numEvents)
+      self.lRatio = 1.0 - self.rRatio  
+    if debug: print self.name,"starting with lRatio:",self.lRatio, "rRatio",self.rRatio
+    
+  def run(self):
+    global baseTime
+    for e in self.specs['events']:
+      while e['time'] > (time.time() - baseTime):
+        time.sleep(0)
+        if self.runState is False:
+          print "%s: aborting"%self.name
+          return
+      path = rootDir + '/' + e['file']
+      print "%s: playing %s"%(self.name,path)
+      sound = pygame.mixer.Sound(file=path)
+      factor = e['factor']
+      nsound = gardenTrack.speedx(sound,factor)
+      if nsound is not None:
+        sound = nsound
+      playSound(sound,self.lRatio,self.rRatio)
+    print "%s: exiting"%self.name
+    
+def waitForThreads():
+  print "waiting for threads to be done"
+  done = False
+  while True:
+    test = []
+    for t in threads:
+      if t.isAlive():
+        test.append(t)
+      else:
+        t.join()
+    if len(test) == 0:
+        break
+    time.sleep(0)
+  print "threads done"
+    
+    
 if __name__ == '__main__':
   pname = sys.argv[0]
   os.environ['DISPLAY']=":0.0"
   os.chdir(os.path.dirname(sys.argv[0]))
   print(pname+" at "+datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))  
-
-  if len (sys.argv) < 2:
-    usage()
-  pygame.mixer.pre_init(frequency=44100, size=-16, channels=1, buffer=4096)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-d', '--dir', nargs=1, help='target directory', required=True )
+  parser.add_argument('-v', '--voices', nargs='*', default=[], help='voice list [defaults to all]')
+  args = parser.parse_args()
+  takesDir = "%s/%s"%(takesDir,args.dir[0])
+  voices = []
+  if len(args.voices) == 0:
+    d = "%s/GardenTrack*json"%takesDir
+    print "adding all voices from",d
+    files = glob.glob(d)
+    for i in range(1,len(files)+1):
+      voices.append(i)
+  else:
+    for v in args.voices:
+      voices.append(int(v))
+  print "voices",voices
+  chans = 2
+  if len(voices) == 1:
+    chans = 1
+  numEvents = len(voices)
+  pygame.mixer.pre_init(frequency=44100, size=-16, channels=chans, buffer=4096)
   pygame.init()
   rootDir = os.environ['GARDEN_ROOT_DIR']
+  
+  # mark
   path = rootDir + '/' + "tudor1.wav"
   sound = pygame.mixer.Sound(file=path)
-  playSound(sound)
+  playSound(sound,1.0,1.0)
   
-  with open(sys.argv[1]) as f:
-    specs = json.load(f)
-  baseTime = time.time()
-  for e in specs['events']:
-    while e['time'] > (time.time() - baseTime):
-      time.sleep(0)
-    print "playing",e['file']
-    path = rootDir + '/' + e['file']
-    sound = pygame.mixer.Sound(file=path)
-    factor = e['factor']
-    nsound = gardenTrack.speedx(sound,factor)
-    if nsound is not None:
-      sound = nsound
-    playSound(sound)  
-  print "waiting for channels to be done"
+  count = 1
+  for i in voices:
+    with open("%s/GardenTrack-%d.json"%(takesDir,i)) as f:
+      specs = json.load(f)
+      f.close()
+      t = Playback(count,specs)
+      count += 1 
+      threads.append(t)
+  for t in threads:
+    t.start()
+  
+  waitForThreads()
+    
   while True:
     n = pygame.mixer.get_busy()
     print "number busy channels",n
     if n == 0:
       break;
     time.sleep(1)
+  
